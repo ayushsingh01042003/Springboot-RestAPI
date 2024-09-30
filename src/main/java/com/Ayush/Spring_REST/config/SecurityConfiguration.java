3460586237e77aaae5a97d2d6db49b7eb99b489a
Ayush Singh <ayushsingh01042003@gmail.com>
2024-09-30 19:57:29 +0530

    CORS configuration

--- src/main/java/com/Ayush/Spring_REST/config/SecurityConfiguration.java
+++ src/main/java/com/Ayush/Spring_REST/config/SecurityConfiguration.java
@@ -16,7 +16,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import com.Ayush.Spring_REST.service.CustomUserDetailsService;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -68,5 +74,17 @@ public class SecurityConfiguration {
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:8080"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
